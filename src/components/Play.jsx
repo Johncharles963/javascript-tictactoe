523d6acc8536844e578ae6934266bdff88238fb9
John Charles <jcharles@breezeline.net>
2023-01-13 18:16:01 -0500

    Render Board

--- src/components/Play.jsx
+++ src/components/Play.jsx
@@ -1,32 +1,57 @@
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col' 
+import Col from 'react-bootstrap/Col'
 import Image from 'react-bootstrap/Image'
-// "https://cdn3.iconfinder.com/data/icons/letters-and-numbers-1/32/letter_O_blue-1024.png"
-// "https://cdn3.iconfinder.com/data/icons/letters-and-numbers-1/32/letter_X_blue-1024.png"
+import {useState, useEffect} from 'react'
+
 
 const Play = () => {
-    const makeSquares = () => {
-        const squareClicked = (index) =>{
-            alert('Clicked ' + index)
-        }
+    const [gameSquares, setGameSquares] = useState()
+    
+    useEffect(() => {
+        addItemsToArray()
+      }, []);
+
+    const addItemsToArray = () => {
         let arr = []
         for (let i = 0; i < 9; i++) {
-            arr.push(<>
-                <button className='btn-remove' onClick={()=>{squareClicked(i)}}><Image fluid src="Letter0.png"/></button>
-            </>)
+            arr.push({img: 'letterO.png', value: ''})
         }
-        return (<>
-            {arr.map((row, index) => { return (<Col key={index} xs={4} className='square'>{row}</Col>) })}
-        </>
-        )
+        setGameSquares(arr)       
+        
     }
+    const squareClicked = (index) => {
+        let arr = [...gameSquares]
+        if(arr[index].value=== 'x')
+        {
+            alert('You have this')
+        }
+        else if(arr[index].value=== 'o')
+        {
+            alert('Enemy has this')
+        }
+        else{
+            arr[index].img = 'letterX.png'
+            arr[index].value = 'x'
+            setGameSquares(arr)
+        }
+    }
+    const addHTML =() =>{
+        let arr = gameSquares
+        for (let i = 0; i < 9; i++) {
+            arr[i].button = (<Col key={i} xs={4} className='square'><button className='btn-remove' onClick={() => { squareClicked(i) }}><Image fluid src={gameSquares[i].img} /></button></Col>)
+        }
+        if(gameSquares !== arr)
+        setGameSquares(arr)
+        }
+    
+    if(gameSquares)addHTML()
     return (
-        <Container className='bg-primary mx-auto align-self-center' style={{width: "700px"}}>
+        <Container className='bg-primary mx-auto align-self-center' style={{ width: "700px" }}>
             <Row>
-            {
-                makeSquares()
-            }
+                {gameSquares &&
+                    gameSquares.map(u => {return u.button})
+                }
             </Row>
         </Container>
     )
